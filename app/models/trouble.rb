f558107a143e72f40605282729563930ef59d48a
河田昂 <eerf0309@gmail.com>
2019-06-29 09:29:35 +0900

    kawata ver1.0

--- app/models/trouble.rb
+++ app/models/trouble.rb
@@ -1,2 +1,13 @@
+require 'carmen'
+include Carmen
+
 class Trouble < ApplicationRecord
+	before_save :change_code_to_name
+
+	def change_code_to_name
+		temp = Country.coded(self.country)
+		self.country = temp.name
+		temp_reg = temp.subregions.coded(self.region)
+		self.region = temp_reg.name
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'troubles/subregion_options' => 'troubles#subregion_options'
   get 'troubles/top'
   get 'troubles/country'
   get 'troubles/region'
   get 'troubles/index'
   get 'troubles/new'
+  post 'troubles/new' => 'troubles#create'
   get 'troubles/create'
+  get 'troubles/:country', to: 'troubles#region'
+  get 'troubles/:country/:region' => 'troubles#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
